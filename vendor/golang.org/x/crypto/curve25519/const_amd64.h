c957af29023c097424482a1ef99a35089678bee6
faf <fafcoin@163.com>
2021-06-13 21:06:34 +0800

    Revert "Revert "faf""
    This reverts commit fa13c7e75be1ac9ebbaa1c3f6a7639158f09278e.

--- vendor/golang.org/x/crypto/curve25519/const_amd64.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 The go-fafjiadong wang
-// This file is part of the go-faf library.
-// The go-faf library is free software: you can redistribute it and/or modify
-
-// This code was translated into a form compatible with 6a from the public
-// domain sources in SUPERCOP: https://bench.cr.yp.to/supercop.html
-
-#define REDMASK51     0x0007FFFFFFFFFFFF
